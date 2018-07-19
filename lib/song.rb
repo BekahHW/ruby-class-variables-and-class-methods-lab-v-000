d381d3232bb8cf08878ae1237e2e8a4e7bd50ede
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-19 11:25:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
    end
     
     def self.genre_count
-      genre_count = {} 
+      genre_count = {}
+      
       @@genres.each do|genre| 
        if genre_count[genre] 
         genre_count[genre] += 1
@@ -41,6 +42,7 @@ class Song
         genre_count[genre] = 1
       end 
     end
+    genre_count
     end
     
     def self.artist_count
