9ad22d4ac07ec93c5ad3ee0ade0eb825d61a9b6f
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-19 10:41:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,16 @@
 
 
 class Song
-  @@genres << genre  
-    @@artists << artist
   attr_accessor :name, :artist, :genre
-    
   
    def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   
-      @@count += 1
-
+    @@count += 1
+    @@genres << genre  
+    @@artists << artist
     
    end
    
